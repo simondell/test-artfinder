6229bc29ccc4abc48e8a37c8bfc5593f65f84b7f
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-05 15:17:25 +0100

    Kick things off with super-basic failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a currency converter app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Currency Converter/i);
   expect(linkElement).toBeInTheDocument();
 });
