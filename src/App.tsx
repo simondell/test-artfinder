11a1b1c9fff07866f754eb524d74243d51dda0aa
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-06 17:49:59 +0100

    Revert "Giving up on typing currency names. This is bullshit."
    This reverts commit 9e6cd6f818d993000501b1abcccb8b44eab38595.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Currency from './Currency/Currency';
-import RatesContext from './Rates/RatesContext'
+import {
+  CurrencyCodes
+} from './Currency/Constants'
 import './App.css';
 
 interface Amount {
@@ -8,47 +10,49 @@ interface Amount {
   value: number;
 }
 
+type Rate = [CurrencyCodes, number]
+
 function App() {
   let amounts: Amount[] = [
-    { denomination: 'USD', value: 0 },
-    { denomination: 'JPY', value: 0 },
+    { denomination: CurrencyCodes.USD, value: 0 },
+    { denomination: CurrencyCodes.JPY, value: 0 },
   ]
 
-  const rates = [
-    ['USD', 1.1250],
-    ['JPY', 122.48],
-    ['BGN', 1.9558],
-    ['CZK', 26.623],
-    ['DKK', 7.4557],
-    ['GBP', 0.89685],
-    ['HUF', 344.90],
-  ]
+  const [rates, setRates] = React.useState<Rate[]>([])
+
+  React.useEffect(() => {
+    setRates([
+      [CurrencyCodes['USD'], 1.1250],
+      [CurrencyCodes['JPY'], 122.48],
+      [CurrencyCodes['BGN'], 1.9558],
+      [CurrencyCodes['CZK'], 26.623],
+      [CurrencyCodes['DKK'], 7.4557],
+      [CurrencyCodes['GBP'], 0.89685],
+      [CurrencyCodes['HUF'], 344.90],
+    ])
+  }, [setRates])
 
   return (
     <div className="App">
-      <RatesContext.Provider
-        value={rates}
+      <header className="App-header">
+        <h1>Currency Converter</h1>
+      </header>
+
+      <section
+        className="converters"
       >
-        <header className="App-header">
-          <h1>Currency Converter</h1>
-        </header>
+        <Currency
+          denomination={amounts[0].denomination}
+          value={amounts[0].value}
+        />
 
-        <section
-          className="converters"
-        >
-          <Currency
-            denomination={amounts[0].denomination}
-            value={amounts[0].value}
-          />
+        <span>to</span>
 
-          <span>to</span>
-
-          <Currency
-            denomination={amounts[1].denomination}
-            value={amounts[1].value}
-          />
-        </section>
-      </RatesContext.Provider>
+        <Currency
+          denomination={amounts[1].denomination}
+          value={amounts[1].value}
+        />
+      </section>
     </div>
   );
 }
