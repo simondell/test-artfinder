2d7056c88dc4338035e569c8d40762bf9a9736da
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-07 18:19:39 +0100

    Add an apology

--- src/Rates/RatesContext.ts
+++ src/Rates/RatesContext.ts
@@ -10,6 +10,9 @@ export function getRate (denomenation: string, rates: Rate[]) {
   return rate[1]
 }
 
+// TODO I'd really like to index these by the rate name string
+//   but TypeScript seems determined to make that tricky for me.
+//   I know this is a flaw in my understanding of what TS does.
 export default React.createContext([
   ['USD', 1.1250],
   ['HUF', 344.90],
